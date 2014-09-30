d032fc3cc4bb728f332b23ece6cb29db6177a69e
KBarros2014 <katieb784@gmail.com>
2014-09-30 22:45:41 +1300

    I can add product created productadmin controller.
    working on productController

--- site/controllers/productsController.php
+++ site/controllers/productsController.php
@@ -1,8 +1,8 @@
 <?php
 /*
-   A PHP framework for web sites by Mike Lopez
+   A PHP framework for web sites by KBarros
    
-   Sample CRUD controller for a list of categories
+   CRUD controller for a list of categories
    ===============================================
    
 */
@@ -16,12 +16,27 @@ class ProductsController extends AbstractController {
 		parent::__construct($context);
 	}
 	protected function getView($isPostback) {
+		$db=$this->getDB();
+		$sql="select productID, productName from products order by productName asc";
+		$rows=$db->query($sql);
+		if (count($rows)==0) {
+			$html='<p>There are no products</p>';
+		} else {
+			$table = new TableView($rows);
+			$table->setColumn('productName','Product name');
+			$table->setColumn('action','Action',
+				'&nbsp;<a href="##site##admin/product/view/<<productID>>">View</a>'.
+				'&nbsp;<a href="##site##admin/product/edit/<<productID>>">Edit</a>'.
+				'&nbsp;<a href="##site##admin/product/delete/<<productID>>">Delete</a>');
+			$html=$table->getHtml();
+			$html.='<p><a href="##site##admin/product/new">Add a new product</a></p>';
 		$view= new View($this->getContext());	
 		$view->setModel(null);
 		$view->setTemplate('html/masterPage.html');
-		$view->setTemplateField('pagename','Categories');
-		$view->addContent('To be written');
+		$view->setTemplateField('pagename','Products');
+		$view->addContent($html);
 		return $view;
 	}
+}
 }
 ?>
