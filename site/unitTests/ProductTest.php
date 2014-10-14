b6b7513d4d394b1afe9481b27b3ce46f5453ff87
Stephen Reynolds <sjr0379@student.cpit.ac.nz>
2014-10-15 11:21:32 +1300

    Updated Product tests

--- site/unitTests/ProductTest.php
+++ site/unitTests/ProductTest.php
@@ -1,5 +1,4 @@
 <?php
-//include 'models/category.php';
 include 'models/product.php';
 
 class ProductTest extends UnitTest {
@@ -11,10 +10,76 @@ class ProductTest extends UnitTest {
 	protected function doTests() {
 		$context=$this->getContext();
 		$db=$context->getDB();
+    
+		// static method tests
+		$OKname= str_repeat('x',64);
+		$longName= str_repeat('x',65);
+		$OKdesc=str_repeat('x',200);
+		$longDesc=str_repeat('x',201);
+        
+        // Checking name
+		$this->assertEqual(ProductModel::errorInProductName(null),'Product name must be specified','name validation');
+		$this->assertEqual(ProductModel::errorInProductName(''),'Product name must be specified','name  validation');
+		$this->assertEqual(ProductModel::errorInProductName($longName),'Product name must have no more than 64 characters','name validation');
+		$this->assertEqual(ProductModel::errorInProductName($OKname),null,'name validation');
+        
+        // Checking description
+        /*
+        //No description implemented yet
+        $this->assertEqual(ProductModel::errorInProductDescription(null),'Product description must be given','Description validation');
+        $this->assertEqual(ProductModel::errorInProductDescription(''),'Product description must be given','Description validation');
+        */
+        
+        $product=new ProductModel($db);
+		$this->assertEqual($product->getProductID(),null,"Default id should be null");
+		$this->assertEqual($product->getProductName(),null,"Default name should be null");
+		// No description implemented yet
+        //$this->assertEqual($product->getDescription(),null,"Default description should be null");
+		
+// insert into products (productName, productDescription, productPrice, productPic, catID) 
+// values ('Product one','Description of product one',123.45,null,1);
 
-		// the following is just a place-holder - delete when tests written
-		$this->assert (false,'Test not yet written');
-	}
+        $product=new ProductModel($db,1);
+        $this->assertEqual($product->getProductID(),'1',"Product 1 id");
+   		$this->assertEqual($product->getName(),'Product one',"Product one name");
+   		$this->assertEqual($product->getProductPrice(),'123.45',"Product  1 price");
+
+   		$product->setProductName('Product 1');
+   		$product->setProductPrice('453.21');
+   		$this->assertEqual($product->getName(),'Product 1',"Product one name");
+   		$this->assertEqual($product->getProductPrice(),'453.21',"Product  1 price");
+        
+        
+        
+
+        //this->assertEqual($product-getProductID()
+
+        /*
+        getProductName
+        setProductName
+        getProductPrice
+        setProductPrice
+        setProductPic
+        hasChanges
+        load
+        save
+        delete
+        errorInProductName
+        errorInProductPrice
+        */
+        
+        /*    
+        $prod = new ProductModel($db);
+        this->assertEqual
+    
+    
+        $prodID = 1;
+        $this->assertEqual($prodID->getProductId
+        getProductId() {
+            return $this->productId;
+        }values ('Product one','Description of product one',123.45,null,1);
+*/
+    }
 }
 ?>
 	
