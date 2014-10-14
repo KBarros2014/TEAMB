0bfb857b481910af8c04ac981ee8c3e562165924
KBarros2014 <katieb784@gmail.com>
2014-10-14 23:03:35 +1300

    a few changes to customer controller
    I will finish it up tomorrow

--- site/controllers/customerController.php
+++ site/controllers/customerController.php
@@ -1,24 +1,7 @@
 <?php
-/*
-   
-   
-   Sample CRUD controller for a customer
-   =============================================
-
-   The following URI patterns are handled by this controller: 
-   
-   /admin/customer/new         	create a new customer
-   /admin/customer/edit/nn		edit customer nn
-   /admin/customer/delete/nn	delete customer nn
-   /admin/customer/view/nn      view customer nn
-   
-   (nn is the customer ID)
-   
- We're just implementing the customer specific stuff
-*/
-
+//14/10/2014 kb
 include 'controllers/crudController.php';
-include 'models/customer.php';
+include 'models/customerModel.php';
 
 class CustomerController extends CrudController {
 
@@ -32,92 +15,91 @@ class CustomerController extends CrudController {
 	
 	// the following methods are the must-overrides in the Crud controller
 	protected function getTemplateForNew () {
-		return 'html/forms/adminCustomerNew.html';
+		return 'html/forms/adminCustomersNew.html';
 	}
 	protected function getTemplateForEdit () {
-		return 'html/forms/adminCustomerEdit.html';
+		return 'html/forms/adminCustomersEdit.html';
 	}
 	protected function getTemplateForDelete () {
-		return 'html/forms/adminCustomerDelete.html';
+		return 'html/forms/adminCustomersDelete.html';
 	}
 	protected function getTemplateForView () {
-		return 'html/forms/adminCustomerView.html';
+		return 'html/forms/adminCustomersView.html';
 	}
 	protected function createModel($id) {
 		return new CustomerModel($this->getDB(),$id);
 	}
 	protected function getModelData($model) {
-		$this->setField('firstname', $model->getcustomerFirstName());
-		$this->setField('lastName',$model->getcustomerLastName());
-		$this->setField('address',$model->getcustomerAddress());
-		$this->setField('city',$model->getcustomerCity());
-		$this->setField('postcode',$model->getcustomerPostCode());
-		$this->setField('email',$model->getcustomerEmail());
-		
-		
+		$this->setField('firstname', $model->getCustomerFirstName());
+		$this->setField('lastName',$model->getCustomerLastName());
+		$this->setField('address',$model->getCustomerAddress());//kb
+		$this->setField('city',$model->getCustomerCity());
+		$this->setField('postcode',$model->getCustomerPostCode());
+		//$this->setField('email',$model->getcustomerEmail());for testing purposes I commented it out kBaros
+		//I need to create the function in customer model
+			
 	}
 	protected function getFormData() {
-		$firstname=$this->getInput('Firstname');
+		$firstname=$this->getInput('firstname');
 		$this->setField('firstname', $firstname);
-		$error=CustomerModel::errorInFirstName($Firstname);
+		$error=CustomerModel::errorInCustomerFirstName($firstname);//function in custmoer modelkb
 		if ($error!==null) {
-			$this->setError ('Firstname',$error);
+			$this->setError ('firstname',$error);
 		}
-		$lastname=$this->getInput('Lastname');
-		$this->setField('lastname', $Lastname);
-		$error=CustomerModel::errorInLastname($Lastname);
+		$lastname=$this->getInput('lastname');
+		$this->setField('lastname', $lastname);
+		$error=CustomerModel::errorInCustomerLastName($lastname);//kb 
+		
 		if ($error!==null) {
-			$this->setError ('Lastname',$error);
+			$this->setError ('firstname',$error);
+			echo "dldld";
 		}
-		$address=$this->getInput('Address');
-		$this->setField('address', $Address);
-		$error=CustomerModel::errorInAddress($Address);
+		
+		$address=$this->getInput('address');
+		$this->setField('address', $address);//kb
+		$error=CustomerModel::errorInCustomerAddress($address);
 		if ($error!==null) {
-			$this->setError ('Address',$error);
+			$this->setError ('address',$error);
 		}
-		$city=$this->getInput('City');
-		$this->setField('city', $City);
-		$error=CustomerModel::errorInCity($City);
+		$city=$this->getInput('city');
+		$this->setField('city', $city);
+		$error=CustomerModel::errorInCustomerCity($city);
 		if ($error!==null) {
-			$this->setError ('City',$error);
+			$this->setError ('city',$error);
 		}
-		$postcode=$this->getInput('PostCode');
-		$this->setField('postcode', $PostCode);
-		$error=CustomerModel::errorInPostCode($PostCode);
+		$postcode=$this->getInput('postcode');//kb
+		$this->setField('postcode', $postcode);
+		$error=CustomerModel::errorInCustomerPostCode($postcode);
 		if ($error!==null) {
-			$this->setError ('PostCode',$error);
+			$this->setError ('postcode',$error);
 		}
-		$email=$this->getInput('Email');
-		$this->setField('email', $Email);
-		$error=CustomerModel::errorInEmail($Email);
+		$email=$this->getInput('email');
+		$this->setField('email', $email);
+		$error=CustomerModel::errorInCustomerEmail($email);
 		if ($error!==null) {
-			$this->setError ('Email',$error);
+			$this->setError ('email',$error);
 		}
 		
 	}
-	protected function updateModel($model) {
-		$firstname=$this->getField('Firstname');
-		$lastname=$this->getField('Lastname');
-		$address=$this->getField('Address');
-		$city=$this->getField('City');
-		$postcode=$this->getField('PostCode');
-		$email=$this->getField('Email');
-		
-		
-		$model->setFirstname($Firstname);
-		$model->setLastname($Lastname);
-		$model->setAddress($Address);
-		$model->setCity($City);
-		$model->setPostcode($PostCode);
-		$model->setEmail($Email);
-		
+	protected function updateModel($model) {//this function gets input boxes asks model if it s right asves it kab
+		$firstname=$this->getField('firstname');//kb
+		$lastname=$this->getField('lastname');
+		$city= $this->getField('city');
+		$address=$this->getField('address');
+		$postcode=$this->getField('postcode');
+		$email=$this->getField('email');//please create a function for getting email kb
+		$model->setCustomerFirstName($firstname);//could simplify wiht one function only kb
+		$model->setCustomerLastName($lastname);	
+		$model->setCustomerCity($city);
+		//$model->setCustomerEmail($email);//to be implemented kb
+		$model->setCustomerPostCode($postcode);
 		$model->save();
-		$this->redirectTo('admin/customers',"Customer '$Firstname' has been saved");
+		$this->redirectTo('admin/customers',"Customer '$firstname' has been saved");//I will change that kb
 	}
 	protected function deleteModel($model) {
-		$firstname=$model->getFirstname();
+		$name=$model->getName();//this does not work yet kb
 		$model->delete();
-		$this->redirectTo('admin/customers',"Customer '$Firstname' has been deleted");
+		$this->redirectTo('admin/customers',"Customer '$name' has been deleted");
 	}	
 }
 ?>
