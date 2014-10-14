77c665075ecee26b157703e62ac93dc1d9605fd4
KBarros2014 <katieb784@gmail.com>
2014-10-14 23:04:49 +1300

    the right syntax it works I wll add html for customer

--- site/controllers/customersController.php
+++ site/controllers/customersController.php
@@ -1,8 +1,8 @@
 <?php
 /*
-  
+   A PHP framework for web sites by Mike Lopez
    
-   Sample CRUD controller for all customers
+   Sample CRUD controller for a list of categories
    ===============================================
    
 */
@@ -10,33 +10,42 @@ include 'lib/abstractController.php';
 include 'lib/view.php';
 include 'lib/tableView.php';
 
-class customersController extends AbstractController {
+class CustomersController extends AbstractController {
 
 	public function __construct($context) {
 		parent::__construct($context);
 	}
 	protected function getView($isPostback) {
 		$db=$this->getDB();
-		$sql="select custID, custFirstName from customers order by custFirstName asc";
+	
+		$sql="select customerId, customerFirstName, customerLastName, customerAddress
+, customerCity, customerPostCode, customerEmail, isBusinessAccount from customers order by customerFirstName asc";
 		$rows=$db->query($sql);
 		if (count($rows)==0) {
 			$html='<p>There are no customers</p>';
+			
 		} else {
 			$table = new TableView($rows);
-			$table->setColumn('custName','Customer firstname');
+			$table->setColumn('customerFirstName','Customer first name');
+			$table->setColumn('customerLastName','Customer last name');
+			$table->setColumn('customerEmail','Customer email');
 			$table->setColumn('action','Action',
-				'&nbsp;<a href="##site##admin/customer/view/<<custID>>">View</a>'.
-				'&nbsp;<a href="##site##admin/customer/edit/<<custID>>">Edit</a>'.
-				'&nbsp;<a href="##site##admin/customer/delete/<<custID>>">Delete</a>');
+				'&nbsp;<a href="##site##admin/customer/view/<<customerId>>">View</a>'.
+				'&nbsp;<a href="##site##admin/customer/edit/<<customerId>>">Edit</a>'.
+				'&nbsp;<a href="##site##admin/customer/delete/<<customerId>>">Delete</a>');
 			$html=$table->getHtml();
 			$html.='<p><a href="##site##admin/customer/new">Add a new customer</a></p>';
+		
+			
 		}	
 		$view= new View($this->getContext());	
 		$view->setModel(null);
 		$view->setTemplate('html/masterPage.html');
-		$view->setTemplateField('pagename','Customers');
+		$view->setTemplateField('pagename','Users');
+		echo "kdkd";//that s me playkb 
 		$view->addContent($html);
 		return $view;
 	}
 }
+
 ?>
