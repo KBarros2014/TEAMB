cffa9a84f93404a15b5aceb1a63745af6ec82680
Alex Zhu <exjudy2@hotmail.com>
2014-11-04 11:19:19 +1300

    update order interfaces

--- site/controllers/orderController.php
+++ site/controllers/orderController.php
@@ -18,16 +18,7 @@ class orderController extends CrudController {
 	}
 	
 	// the following methods are the must-overrides in the Crud controller
-	/*protected function getTemplateForNew () {
-		return 'html/forms/adminCategoryNew.html';
-	}
-	protected function getTemplateForEdit () {
-		return 'html/forms/adminCategoryEdit.html';
-	}
-	protected function getTemplateForDelete () {
-		return 'html/forms/adminCategoryDelete.html';
-	}
-	*/
+	
 	protected function getTemplateForNew () {
 		//return 'html/forms/adminOrderNew.html';
 	}
@@ -46,37 +37,16 @@ class orderController extends CrudController {
 	}
 	
 	protected function getModelData($model) {
-		/*$this->setField('OrderName', $model->getName());
-		$this->setField('orderPRice',$model->getDescription());	*/	
+		
 	}
 	protected function getFormData() {
-		/*$name=$this->getInput('name');
-		$this->setField('name', $name);
-		$error=CategoryModel::errorInName($name);
-		if ($error!==null) {
-			$this->setError ('name',$error);
-		}
-		$description=$this->getInput('description');
-		$this->setField('description', $description);
-		$error=CategoryModel::errorInDescription($description);
-		if ($error!==null) {
-			$this->setError ('description',$error);
-		}*/
+		
 	}
 	protected function updateModel($model) {
-		/*$name=$this->getField('name');
-		$description=$this->getField('description');
 		
-		$model->setName($name);
-		$model->setDescription($description);	
-		$model->save();
-		$this->redirectTo('admin/categories',"Category '$name' has been saved");*/
 	}
 	protected function deleteModel($model) {
-	/*
-		$name=$model->getName();
-		$model->delete();
-		$this->redirectTo('admin/categories',"Category '$name' has been deleted");*/
+	
 	}
 }
 ?>
--- site/models/orderModel.php
+++ site/models/orderModel.php
@@ -45,56 +45,15 @@ class orderModel extends  AbstractModel{
 	}
 	
 	private function load($orderId) {
-		$sql="select customerFirstName, customerLastName, customerAddress, customerEmail  from customers ".
-			 "where customerId = $customerId";
-		$rows=$this->getDB()->query($sql);
-		if (count($rows)!==1) {
-			throw new InvalidDataException("Customer  ($customerId) not found");
-		}
-		$row=$rows[0];
-		$this->customerFirstName=$row['customerFirstName'];
-		$this->customerLastName=$row['customerLastName'];
-		$this->customerAddress=$row['customerAddress'];
-		$this->customerEmail=$row['customerEmail'];
-		$this->customerId=$customerId;
-		$this->changed=false;
+	
 	}
 	
 	public function save() {//the save function is to be fixed
-		$id=$this->customerId;
-		if ($this->customerFirstName==null || $this->customerLastName==null|| $this->customerAddress==null || $this->customerEmail==null) {
-			throw new InvalidDataException('Incomplete data');
-		}
-		$firstName=$this->customerFirstName;
-		$lastName=$this->customerLastName;
-		$address=$this->customerAddress;
-		$city = $this->customerCity;
-		$postCode = $this->customerPostCode;
-		$email =$this->customerEmail;
 		
-		if ($this->id===null) {
-			$sql="insert into customers (customerFirstName, customerLastName, customerAddress, customerCity, customerEmail, customerEmail) values (".
-						"'$firstName', '$lastName','$$address', '$city','$postCode', '$email')";
-			$this->getDB()->execute($sql);
-			$this->id=getDB()->insertID();	
-		} else {
-			$sql="update customers ".
-					"set customerFirstName='$firstName', ".
-			            "customerLastName='$lastName' ".
-						 "customerAddress='$address' ".
-						  "customerCity='$city' ".
-						   "customerPostCode='$postCode' ".
-						"where customerId= $customerID";
-			$this->getDB()->execute($sql);
-		}
-		$this->hasChanges=false;
 	}
 	
 	public function delete () {
-	    $sql="delete from customers where customerId = $id";
-		$rows=$this->getDB()->execute($sql);
-		$this->id=$null;
-		$this->changed=false;
+	    
 	}
 	
 	}
