d7b625c9e5cb32eab95097df8e7ec16129ecd704
naicygeorge <georgenaicy21@gmail.com>
2014-11-11 16:28:48 +1300

    checkout controller
    need corrections

--- site/controllers/checkoutController.php
+++ site/controllers/checkoutController.php
@@ -1,23 +1,49 @@
 <?php
-include 'models/myShoppingCart.php';
-include 'models/orderModel.php';
+include 'controllers/crudController.php';
+include 'models/ShoppingCart.php'; 
 
-class CheckoutController extends AbstractController {
+class CheckoutController extends CrudController {
 
-	public function __construct($context) {
-		parent::__construct($context);
+	public function __construct(IContext $context) {
+		parent::__construct($context);  
 	}
-	protected function getView($isPostback) {
-		
-		// test data
-		// 1 and 2 are productIDs
-		$cart = new ShoppingCart();
-		$cart->addItem (new ShoppingCartItem(1,20,45.95));
-		$cart->addItem (new ShoppingCartItem(2,10,45.95));
-		
-		$customerID=5; // tests data
-		
-		OrderModel::createFromCart($cart, $customerID);
+
+	protected function getPagename(){
+		return 'Products';
+	}
+
+	protected function getTemplateForNew () {
+		return 'html/forms/adminProductNew.html';
+	}
+	
+	protected function getTemplateForEdit () {
+		return 'html/forms/adminProductEdit.html';
+	}
+	
+	protected function getTemplateForDelete () {
+		return 'html/forms/adminProductDelete.html';
+	}
+	
+	protected function getTemplateForView () {
+		return 'html/forms/adminProductView.html';
+	}
+
+	protected function createModel($CustomerId) {
+		return new ShoppingCartModel($this->getDB(),$CustomerId);
+	}
+	protected function getModelData($model) {
+		$this->setField('id', $model->getProductId());
+		$this->setField('quantity',$model->getProductQuantity());
+		$this->setField('price',$model->getProductPrice());
+	}
+	protected function getFormData() {
 		
 	}
+	protected function updateModel($model) {
+		
+	}
+	protected function deleteModel($model) {
+		
+	}	
+}
 ?>
