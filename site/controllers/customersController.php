970898d711a399e748512533ac5ecfe8b928a3b5
Alex Zhu <exjudy2@hotmail.com>
2014-11-20 18:45:29 +1300

    page limit on , working on customer, fix bug

--- site/controllers/customersController.php
+++ site/controllers/customersController.php
@@ -16,10 +16,33 @@ class CustomersController extends AbstractController {
 		parent::__construct($context);
 	}
 	protected function getView($isPostback) {
+		
+		$uri=$this->getURI();
+		$part = $uri->getPart();
+		$db=$this->getDB();	
+		$limit=5;	
+		$totalPages=10;
+		if ($part==="") {
+			$rows= $db->query("select count(customerId) as totalCount from customers");
+			$row=$rows[0];
+			$rowCount =$row['totalCount'];
+			$totalPages=ceil ($rowCount / $limit); // total number of pages
+		}
+		if (is_numeric($part)) {
+			$pageNumber = (int) $part;
+			$part2= $uri->getPart();
+				if (is_numeric($part2)) {
+					$totalPages = (int) $part2;
+				}
+		} else {
+		$pageNumber=1;
+		}
+		$offset=($pageNumber - 1) * $limit;
+		
 		$db=$this->getDB();
 	
 		$sql="select customerId, customerFirstName, customerLastName, customerAddress
-, customerCity, customerPostCode, customerEmail, isBusinessAccount from customers order by customerFirstName asc";
+, customerCity, customerPostCode, customerEmail, isBusinessAccount from customers order by customerFirstName asc Limit $limit offset $offset";
 		$rows=$db->query($sql);
 		if (count($rows)==0) {
 			$html='<p>There are no customers</p>';
@@ -35,14 +58,15 @@ class CustomersController extends AbstractController {
 				'&nbsp;<a href="##site##admin/customer/delete/<<customerId>>">Delete</a>');
 			$html=$table->getHtml();
 			$html.='<p><a href="##site##admin/customer/new">Add a new customer</a></p>';
-		
+		for ($i=1; $i <= $totalPages; $i++ ) {
+			$html .="<a href=\"##site##admin/categories/$i\" > $i </a>";
 			
+		}		
 		}	
 		$view= new View($this->getContext());	
 		$view->setModel(null);
 		$view->setTemplate('html/masterPage.html');
-		$view->setTemplateField('pagename','Users');
-		echo "kdkd";//that s me playkb 
+		$view->setTemplateField('pagename','Customers');
 		$view->addContent($html);
 		return $view;
 	}
